14dc22c09aedde282eb61198068586b7d4261b7d
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2024-10-29 13:57:37 +0200

    lint error fix

--- lib/chess.rb
+++ lib/chess.rb
@@ -4,14 +4,23 @@
 class Chess
   attr_reader :board
 
-  UPPER_LOWER_BOARD = " abcdefgh \n"
+  UPPER_LOWER_BOARD = " abcdefgh\n"
 
   def initialize(starting_position = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
     @board = Array.new(8) { Array.new(8) }
-    set_pieces(starting_position)
+    place_pieces(starting_position)
   end
 
-  def set_pieces(starting_position)
+  # '♖♘♗♕♔♗♘♖
+  # ♙♙♙♙♙♙♙♙
+  # ■□■□■□■□
+  # □■□■□■□■
+  # ■□■□■□■□
+  # □■□■□■□■
+  # ♟♟♟♟♟♟♟♟
+  # ♜♞♝♛♚♝♞♜'
+
+  def place_pieces(starting_position) # rubocop:disable Metrics/MethodLength
     starting_position.split('/').each_with_index do |rank, i|
       file = 0
       rank.each_char do |char|
