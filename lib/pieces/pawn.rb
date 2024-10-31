25efbb0a92864589097c157383bff4d45d158c33
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2024-10-31 14:02:18 +0200

    pawn front_moves and spec

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -4,8 +4,35 @@ require_relative 'piece'
 
 # Pawn class
 class Pawn < Piece
-  def valid_moves
+  def first_move?
+    position[0] == (color == 'white' ? 6 : 1)
+  end
+
+  def moves
     # Logic for calculating valid moves for a pawn
+    front_moves
+  end
+
+  def front_moves
+    moves = []
+    direction = color == 'white' ? -1 : 1
+
+    if board[position[0] + direction][position[1]].nil?
+      moves << [position[0] + direction, position[1]]
+      if board[position[0] + direction][position[1]].nil? && first_move?
+        moves << [position[0] + (direction * 2), position[1]]
+      end
+    end
+
+    moves
+  end
+
+  def move(destination)
+    raise ArgumentError, 'Invalid move' unless moves.include?(destination)
+
+    board[position[0]][position[1]] = nil
+    self.position = destination
+    board[position[0]][position[1]] = self
   end
 
   def to_s
--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -18,4 +18,57 @@ RSpec.describe Pawn do
       expect(pawn.to_s).to eq('♙')
     end
   end
+
+  describe '#first_move?' do
+    let(:board) { Chess.new.board }
+
+    it 'returns true for a pawn in its starting position' do
+      pawn = described_class.new(board, [6, 1], 'white')
+      expect(pawn.first_move?).to be true
+    end
+
+    it 'returns false for a pawn not in its starting position' do
+      pawn = described_class.new(board, [5, 1], 'white')
+      expect(pawn.first_move?).to be false
+    end
+  end
+
+  describe '#front_moves' do
+    let(:board) { Chess.new.board }
+
+    it 'returns the correct moves for a white pawn in its starting position' do
+      pawn = described_class.new(board, [6, 1], 'white')
+      expect(pawn.front_moves).to eq([[5, 1], [4, 1]])
+    end
+
+    it 'returns the correct moves for a black pawn in its starting position' do
+      pawn = described_class.new(board, [1, 1], 'black')
+      expect(pawn.front_moves).to eq([[2, 1], [3, 1]])
+    end
+
+    it 'returns the correct moves for a white pawn that has moved' do
+      pawn = described_class.new(board, [5, 1], 'white')
+      expect(pawn.front_moves).to eq([[4, 1]])
+    end
+
+    it 'returns the correct moves for a black pawn that has moved' do
+      pawn = described_class.new(board, [2, 1], 'black')
+      expect(pawn.front_moves).to eq([[3, 1]])
+    end
+  end
+
+  describe '#move' do
+    let(:board) { Chess.new.board }
+
+    it 'raises an error if the move is invalid' do
+      pawn = described_class.new(board, [6, 1], 'white')
+      expect { pawn.move([4, 2]) }.to raise_error(ArgumentError, 'Invalid move')
+    end
+
+    it 'moves the pawn to the correct position' do
+      pawn = described_class.new(board, [6, 1], 'white')
+      pawn.move([5, 1])
+      expect(board[5][1]).to eq(pawn)
+    end
+  end
 end
