dbe9e2b35f786c8edd9b49f8e82b5d4db52385a9
Ignas Vaitkus <ignas.vaitkus7@gmail.com>
2024-10-31 11:10:54 +0200

    input validation and pick_piece method

--- lib/chess.rb
+++ lib/chess.rb
@@ -78,10 +78,32 @@ class Chess
 
   def play_turn
     print_turn_info
+    input = gets.chomp
+    handle_input?(input)
   rescue StandardError => e
     self.retries += 1
     self.message = e
     retry
+  end
+
+  def valid_coordinate_notation_move(move)
+    return if move.match?(/^[A-H][1-8]-[A-H][1-8]$/)
+
+    raise ArgumentError, 'Invalid input, use coordinate notation, for example "A2-A4", "B1-C3", etc.'
+  end
+
+  def handle_input?(move)
+    valid_coordinate_notation_move(move)
+    pick_piece(move)
+  end
+
+  def pick_piece(move)
+    from, = move.split('-')
+    piece = board[8 - from[1].to_i][from[0].ord - 65]
+    raise 'No piece at that position.' if piece.nil?
+    raise 'Not your piece.' if piece.color != current_player
+
+    piece
   end
 
   public
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -26,7 +26,37 @@ describe Chess do
     it 'prints the turn info' do
       expect do
         described_class.new.send(:print_turn_info)
-      end.to output(" abcdefgh\n8♖♘♗♕♔♗♘♖8\n7♙♙♙♙♙♙♙♙7\n6■□■□■□■□6\n5□■□■□■□■5\n4■□■□■□■□4\n3□■□■□■□■3\n2♟︎♟︎♟︎♟︎♟︎♟︎♟︎♟︎2\n1♜♞♝♛♚♝♞♜1\n abcdefgh\n\n\n\nWhite, enter your move:\n\n").to_stdout
+      end.to output(" abcdefgh\n8♖♘♗♕♔♗♘♖8\n7♙♙♙♙♙♙♙♙7\n6■□■□■□■□6\n5□■□■□■□■5\n4■□■□■□■□4\n3□■□■□■□■3\n2♟︎♟︎♟︎♟︎♟︎♟︎♟︎♟︎2\n1♜♞♝♛♚♝♞♜1\n abcdefgh\n\n\n\nWhite, enter your move:\n\n").to_stdout # rubocop:disable Layout/LineLength
+    end
+  end
+
+  describe 'valid_coordinate_notation_move' do
+    it 'returns nil for a valid move' do
+      expect(described_class.new.send(:valid_coordinate_notation_move, 'E2-E6')).to be_nil
+    end
+
+    it 'raises an error for an invalid move' do
+      expect do
+        described_class.new.send(:valid_coordinate_notation_move, 'e2e5')
+      end.to raise_error(ArgumentError, 'Invalid input, use coordinate notation, for example "A2-A4", "B1-C3", etc.')
+    end
+  end
+
+  describe 'pick_piece' do
+    it 'returns the piece at the given position' do
+      expect(described_class.new.send(:pick_piece, 'E2-E4')).to be_instance_of(Pawn)
+    end
+
+    it 'raises an error if there is no piece at the given position' do
+      expect do
+        described_class.new.send(:pick_piece, 'E3-E4')
+      end.to raise_error('No piece at that position.')
+    end
+
+    it 'raises an error if the piece at the given position is not the current player\'s' do
+      expect do
+        described_class.new.send(:pick_piece, 'E7-E5')
+      end.to raise_error('Not your piece.')
     end
   end
 end
